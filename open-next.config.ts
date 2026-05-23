e79b603defe3fbb502ce3f386d52797aec0cfd1a
Sunil Shah <sunilshah2416@gmail.com>
2026-05-23 16:14:40 -0400

    redirected build from opennext cloudflare to next build to keep it out of infinite loop

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,4 +3,5 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 export default defineCloudflareConfig({
   // For best results consider enabling R2 caching:
   // https://opennext.js.org/cloudflare/caching
+	buildCommand: "npm run build:next",
 });
